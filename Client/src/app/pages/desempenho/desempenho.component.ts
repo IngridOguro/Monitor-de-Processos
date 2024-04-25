a5408f17090246afe9618e35c4802a6925709fc3
IS Oguro <ingrid.shizue@gmail.com>
2024-04-25 13:31:01 -0300

    organização do layout da pagina de desempenho

--- Client/src/app/pages/desempenho/desempenho.component.ts
+++ Client/src/app/pages/desempenho/desempenho.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { CpuComponent } from '../../components/charts/cpu/cpu.component';
 import { SideBarComponent } from '../../components/bars/side-bar/side-bar.component';
+import { NetComponent } from '../../components/charts/net/net.component';
+import { DiscComponent } from '../../components/charts/disc/disc.component';
+import { MemoryComponent } from '../../components/charts/memory/memory.component';
 
 @Component({
   selector: 'app-desempenho',
   standalone:true,
-  imports:[CpuComponent,SideBarComponent],
+  imports:[CpuComponent,NetComponent,DiscComponent,MemoryComponent,SideBarComponent],
   templateUrl: './desempenho.component.html',
   styleUrls: ['./desempenho.component.css']
 })
