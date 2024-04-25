ab8e3601d71be0a1973b02a311e5d94526792029
IS Oguro <ingrid.shizue@gmail.com>
2024-04-24 23:23:13 -0300

    pagina de desempenho criada

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { DesempenhoComponent } from './components/desempenho/desempenho.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"", component: AppComponent}, //processos
+  {path:"desempenho", component: DesempenhoComponent}, //desempenho
+];
---
+++ Client/src/app/components/desempenho/desempenho.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SideBarComponent } from '../side-bar/side-bar.component';
+
+@Component({
+  selector: 'app-desempenho',
+  standalone: true,
+  imports: [SideBarComponent],
+  templateUrl: './desempenho.component.html',
+  styleUrl: './desempenho.component.css'
+})
+export class DesempenhoComponent {
+
+}
