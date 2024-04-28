e0557afc81480447ffe2462164842e40a7dc5894
IS Oguro <ingrid.shizue@gmail.com>
2024-04-28 06:33:23 -0300

    separação da tabela

--- Client/src/app/pages/processos/processos.component.ts
+++ Client/src/app/pages/processos/processos.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { SideBarComponent } from '../../components/bars/side-bar/side-bar.component';
 import { NavbarComponent } from '../../components/bars/navbar/navbar.component';
 import { ActionBarComponent } from '../../components/bars/action-bar/action-bar.component';
+import { TableComponent } from '../../components/table/table.component';
 
 @Component({
   selector: 'app-processos',
   standalone: true,
-  imports: [SideBarComponent,NavbarComponent,ActionBarComponent],
+  imports: [SideBarComponent,NavbarComponent,ActionBarComponent, TableComponent],
   templateUrl: './processos.component.html',
   styleUrl: './processos.component.css'
 })
